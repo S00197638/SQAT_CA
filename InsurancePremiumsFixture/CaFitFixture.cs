cc8f227a20f081f3749873e0d1d6af58616612e2
Jake Beecham <60218222+JakeBeecham@users.noreply.github.com>
2021-04-12 20:39:52 +0100

    SQAT_CA Adding Fitnesse.

---
+++ InsurancePremiumsFixture/CaFitFixture.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using fitnesse.fixtures;
+using InsurancePremiums;
+
+namespace InsurancePremiumsFixture
+{
+    public class CaFitFixture : fit.ColumnFixture
+    {
+        public int age;
+        public string gender;
+
+        public float calcPremium()
+        {
+            InsuranceService service = new InsuranceService();
+            return service.CalcPremium(age, gender);
+        }
+    }
+}
